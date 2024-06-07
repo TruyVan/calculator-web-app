c0adcbaf036c24710570eff6f2ff588e5366d1de
Truy Van <Truyvanlangkhach2512@gmail.com>
2024-06-07 23:27:00 +0700

    update the work (wrong project)

--- src/App.js
+++ src/App.js
@@ -1,84 +1,41 @@
-import React, {useRef, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 function App(){
   // Components
-  const list = []
   // HOOK REACT
-  const [task, setTask] = useState('')
-  const [tasks, setTasks] = useState([])
-  const [popup, setPopup] = useState(false)
-  const [msg, setMsg] = useState('')
   // Function
-  const getInput = ()=>{
-    if(task !=''){
-      if(tasks.includes(task)){
-        setMsg("You have typed the same task! Please enter the other one.")
-      }
-      else{
-        setTasks(prev => [...prev, task])
-        setTask('')
-        setMsg("")
-      }
-    }
-    else{
-      setMsg("You haven't typed anything! Please enter your note.")
-    }
-  }
-  const handleEnter = (key)=>{
-    if(key === "Enter"){
-      getInput();
-    }
-  }
-  const onDel = (kw)=>{
-    setTasks(prev => prev.filter(item => item != kw))
-  }
-  const onDelConfirm = ()=>{
-    setPopup(!popup);
-  }
-  const onDelAll = ()=>{
-    setTasks([])
-    setPopup(!popup)
-  }
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '.']
+  const calculations = ['+', '–', '×', '÷']
   return(
     <React.Fragment>
-      <div className='container'>
-        <div className='d-flex justify-content-center my-3'>
-          <h1>To-Do List</h1>
+      <div className='container border border-info'>
+        <div className='cal-screen bg-dark d-flex justify-content-end px-2 p-1'>
+          <h4 className='text-white'>123</h4>
         </div>
-        <label>Type your task</label>
-        <div className='input-group'>
-          <input onKeyDown={(e)=>handleEnter(e.key)} value={task} onChange={e => setTask(e.target.value)} className='form-control'></input>
-          <button onClick={()=>getInput()} className='btn btn-primary'>Create</button>
-        </div>
-        <p className='my-2 text-danger'>{msg}</p>
-        <div className='mt-3'>
-          <strong><h4>Here is your task</h4></strong>
-          <ul className='list-group'>
-            {tasks.map(item => <li className='list-group-item'><i onClick={()=>onDel(item)} className='me-3 fas text-danger fa-trash'></i>{item}</li>)}
-          </ul>
-          {tasks.length > 1 &&
-          <div className='d-flex justify-content-end mt-3'>
-            <button onClick={() =>onDelConfirm()} className='btn btn-danger'>Clear All</button>
-          </div>}
-        </div>
-      </div>
-      {
-        popup === true && 
-        <div id='modal-fake' className='container-fluid bg-dark'>
-          <div className='mt-5 container p-0 bg-white'>
-            <div className='modal-header bg-danger text-white'>
-              <h4>ARE YOU SURE YOU WANT TO CLEAR ALL THE TASK?</h4>
+        <div className='cal-keyboard row'>
+          <div className='key-number d-flex flex-row row col-9'>
+            {numbers.map(number=>(
+            <div className='col-4 my-1'>
+              <a target='_blank' href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+                <button  className='btn btn-light col-12'>{number}</button>
+              </a>
             </div>
-            <div className='modal-body'>
-              This action can not be reversed!
-            </div>
-            <div className='modal-action px-3 py-4 d-flex justify-content-end'>
-              <button className='btn btn-light border me-3' onClick={onDelConfirm}>Cancel</button>
-              <button className='btn btn-danger' onClick={()=>onDelAll()}>Clear</button>
+            ))}
+            <div className='col-4 my-1'>
+              <button className='btn btn-primary col-12'>=</button>
             </div>
           </div>
+          <div className='key-calculation col-3'>
+              {calculations.map(item=>(
+              <div className='col-12 my-2'>
+                <a target='_blank' href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+                  <button className='btn btn-secondary opacity-25 col-12'>{item}</button>
+                </a>
+              </div>
+              ))}
+            </div>
         </div>
-      }
+      </div>
     </React.Fragment>
   )
 }
