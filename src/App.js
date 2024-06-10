80575ac78fe95ef30fb6e1f8c2f8216a0e4ad766
Truy Van <Truyvanlangkhach2512@gmail.com>
2024-06-10 22:22:48 +0700

    Update Clear - Brackets - % - All Clear functions

--- src/App.js
+++ src/App.js
@@ -6,23 +6,48 @@ function App(){
   const calculations = ['+', '-', '*', '/']
 
   // HOOK REACT
-  // show what factor 1 got
+  // Show warning Message
+  const [msg, setMsg] = useState('')
+  // Closing or Opening "(round brackets)"
+  const [cRB, setCRB] = useState(false)
+  // show what had typed
   const [show, setShow] = useState(0)
   // get what numbers are getting
   const [get, setGet] = useState(0)
   // Detect Keydown on physical keyboard
   useEffect(() => {
     // window.addEventListener('keydown', onHandleKeyDown);
-    window.addEventListener('keydown', deny());
+    window.addEventListener('keydown', deny);
     return () => {
       // window.removeEventListener('keydown', onHandleKeyDown);
-      window.removeEventListener('keydown', deny());
+      window.removeEventListener('keydown', deny);
     };
   }, []);
   // Function
-  const deny = ()=>{
-    console.error("YOU CAN NOT USE KEYBOARD TO ENTER DATA")
+  //Should we close the round bracket?
+  const onClosingBracket = ()=>{
+    if(!cRB){
+      onHandleGet('(')
+      setCRB(!cRB)
+    }
+    else{
+      onHandleGet(')')
+      setCRB(!cRB)
+    }
   }
+  // Handle Clear one by one
+  const onClearOne = ()=>{
+    if(get.length > 1){
+      setGet(prev => prev.slice(0, -1))
+    }
+    else{
+      setGet(0)
+    }
+  }
+  const deny = ()=>{ //Remove keydown from physical keyboards
+    setMsg("YOU CAN NOT USE KEYBOARD TO ENTER DATA")
+  }
+
   const onHandleCal = ()=>{ //Handle clicking math symbol on screen
     try{
       setShow(get);
@@ -39,37 +64,38 @@ function App(){
     setGet(prev => prev + something)
   }
   const onHandleKeyDown = (event)=>{ // Handle pressing
+    setMsg('');
     var keypress = event
     var dataType = typeof(keypress);
     if(dataType === 'string'){ //Handle Point Symbol on Virtual Keyboards
       onHandleGet(keypress)
     }
-    else if(dataType === 'object'){ //Handle Physical Key Down
-      keypress = keypress.key
-      if(calculations.includes(keypress) || keypress === '.' || !isNaN(keypress)){
-        onHandleGet(keypress);
-      }
-      else if(keypress === 'Enter'){
-        onHandleCal();
-        console.log(get)
-        console.log(show)
-      }
-      else if(keypress === 'Backspace'){
-        if(get === ''){
-          setGet(0);
-        }
-        else{
-          setGet(prev => prev.slice(0, -1))
-        }
-      }
-      else if(keypress === 'Escape'){
-        setGet(0)
-        setShow(0)
-      }
-      else{
-        console.log('Not here')
-      }
-    }
+    // else if(dataType === 'object'){ //Handle Physical Key Down
+    //   keypress = keypress.key
+    //   if(calculations.includes(keypress) || keypress === '.' || !isNaN(keypress)){
+    //     onHandleGet(keypress);
+    //   }
+    //   else if(keypress === 'Enter'){
+    //     onHandleCal();
+    //     console.log(get)
+    //     console.log(show)
+    //   }
+    //   else if(keypress === 'Backspace'){
+    //     if(get === ''){
+    //       setGet(0);
+    //     }
+    //     else{
+    //       setGet(prev => prev.slice(0, -1))
+    //     }
+    //   }
+    //   else if(keypress === 'Escape'){
+    //     setGet(0)
+    //     setShow(0)
+    //   }
+    //   else{
+    //     console.log('Not here')
+    //   }
+    // }
     else if(dataType === 'number'){ // Handle Virtual Key Down
       onHandleGet(String(keypress))
     }
@@ -79,7 +105,7 @@ function App(){
       <div className='container text-center my-4'>
         <h3>The simple calculator</h3>
       </div>
-      <div className='container p-3 bg-secondary border rounded border-dark'>
+      <div className='container p-3 cal-bg border rounded border-dark'>
         <div className='cal-screen bg-dark rounded px-2 py-1'>
           <div className='text-white above-cal-screen d-flex justify-content-end'>
             <h5>{show}</h5>
@@ -87,22 +113,43 @@ function App(){
           <div className='text-white below-cal-screen d-flex justify-content-end'>
             <h1>{get}</h1>
           </div>
+          <div>
+            <h4 className='text-danger'><strong>{msg}</strong></h4>
+          </div>
         </div>
-        <div className='cal-keyboard row'>
+        <div className='row'>
+          <div className='d-flex flex-row row col-9'>
+            <div className='col-4'>
+              <button onClick={()=>onClosingBracket()} className='btn btn-warning border-dark col-12'>(   )</button>
+            </div>
+            <div className='col-4'>
+              <button onClick={()=>onHandleGet('%')} className='btn btn-warning border-dark col-12'>%</button>
+            </div>
+            <div className='col-4'>
+              <button onClick={()=>{onClearOne()}} className='btn btn-warning border-dark col-12'>C</button>
+            </div>
+          </div>
+          <div className='col-3'>
+            <div >
+                <button onClick={()=>{setGet(0);setShow(0);setCRB(false)}} className='btn btn-warning border-dark col-12'>AC</button>
+              </div>
+          </div>
+        </div>
+        <div className='cal-keyboard row '>
           <div className='key-number d-flex flex-row row col-9'>
             {numbers.map(number=>(
             <div key={number} className='col-4 my-1'>
-              <button onClick={()=>{onHandleKeyDown(number)}} className='btn btn-light col-12'>{number}</button>
+              <button onClick={()=>{onHandleKeyDown(number)}} className='border-dark btn btn-light col-12'>{number}</button>
             </div>
             ))}
             <div className='col-4 my-1'>
-              <button onClick={()=>{onHandleCal()}} className='btn btn-primary col-12'>=</button>
+              <button onClick={()=>{onHandleCal()}} className='border-dark btn btn-primary col-12'>=</button>
             </div>
           </div>
           <div className='key-calculation col-3'>
               {calculations.map(item=>(
               <div key={item}  className='col-12 my-2'>
-                <button onClick={()=>onHandleGet(item)} className='btn border btn-secondary opacity-100 col-12'>{item}</button>
+                <button onClick={()=>onHandleGet(item)} className='border-dark btn border btn-secondary opacity-100 col-12'>{item}</button>
               </div>
               ))}
             </div>
